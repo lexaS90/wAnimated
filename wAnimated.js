677cd9ce6b65b73234457f7591578348e12c9cd6
Alexey Stupakov <workinweb24@gmail.com>
2016-07-14 11:37:01 +0300

    add duration, delay, callback_function

--- wAnimated.js
+++ wAnimated.js
@@ -1,15 +1,46 @@
-/*
- * $(".section_2").wAnimated("bounceInRight", "200");
+/* 
+ * wAnimated(effect, offset, duration, delay, callback_function(){});
+ * $(".section_2").wAnimated("fadeIn", "80%", "1s", "300ms",function(th){
+	
+ });
  */
 (function($) {
-	$.fn.wAnimated = function(effect, offset) {
+	$.fn.wAnimated = function(effect, offset, duration, delay, fun) {
+
+		var delay = delay || 0
 		var th = this;
-		$(th).css("opacity", "0").addClass("animated").waypoint({
-			handler: function(direction) {				
-				$(th).addClass(effect).css("opacity", "1");				
+
+		$(th).css("opacity", "0").addClass("animated"); 
+		
+		if (duration){
+			$(th).css({
+				'-webkit-animation-duration':  duration,
+				'-moz-animation-duration':     duration,
+				'animation-duration':          duration
+			});
+		}
+		if (delay){
+			$(th).css({
+				'-webkit-animation-delay':  delay,
+				'-moz-animation-delay':     delay,
+				'animation-delay':          delay
+			});
+		}
+
+		var waypoint = new Waypoint({
+			element: $(th)[0],
+
+			handler: function(direction) {console.log("ok");
+				if (fun)
+					fun($(th));
+				if (effect)
+					$(th).addClass(effect).css("opacity", "1");
+
+				this.destroy();
 			},
-			offset: offset
-				
+
+			offset:offset
+
 		});
 	};
 })(jQuery);
